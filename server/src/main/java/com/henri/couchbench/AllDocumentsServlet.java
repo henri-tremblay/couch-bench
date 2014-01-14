f7c2be4b6b8b7442406cfee2df36dabb73c38a27
Henri Tremblay <henri.tremblay@gmail.com>
2014-01-14 01:49:44 +0100

    Get all to retrieve all ids

--- server/src/main/java/com/henri/couchbench/AllDocumentsServlet.java
+++ server/src/main/java/com/henri/couchbench/AllDocumentsServlet.java
@@ -1,11 +1,14 @@
 package com.henri.couchbench;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import net.sf.json.JSONObject;
 
 import com.fourspaces.couchdb.Database;
 import com.fourspaces.couchdb.Session;
@@ -22,8 +25,16 @@ public class AllDocumentsServlet extends HttpServlet {
 		PrintWriter out = res.getWriter();
 
 		res.setContentType("text/plain");
-		for(Object doc : results.entrySet()) {
-		    out.println(doc);
+		@SuppressWarnings("unchecked")
+    List<JSONObject> rows = (List<JSONObject>) results.get("rows");
+		int i  = 0;
+		for(JSONObject doc : rows) {
+		    String line = i + ":" + doc.getString("id");
+		    out.println(line);
+		    i++;
+        if(i % 100 == 0) {
+          out.flush();
+        }
 		}
 		out.close();
 	}
